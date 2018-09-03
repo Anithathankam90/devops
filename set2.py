5e3c8fd3318bfecf41974f9647d69a72c3df9909
anithathankam90 <anithathankam90@gmail.com>
2018-09-03 19:55:25 +0530

    new branch

---
+++ set2.py
@@ -0,0 +1,10 @@
+
+animal = {'cat', 'dog', 'rabbit', 'guinea pig'}
+
+print("initial set:" , animal)
+# Deleting 'fish' element
+animal.remove('cat')
+
+
+# Updated animal
+print('Updated animal set: ', animal)
